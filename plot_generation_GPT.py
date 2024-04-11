83206b30229ee1b2f6f33ad5e4a9751c5c39083c
galtimur <gal_timur@yahoo.com>
2024-04-11 10:48:25 +0200

    Added a script for plot building a plot for existing datapoints.

---
+++ plot_generation_GPT.py
@@ -0,0 +1,96 @@
+from omegaconf import OmegaConf
+from pathlib import Path
+from tqdm import tqdm
+import json
+import glob
+import os
+import time
+import re
+import random
+from dataclasses import dataclass
+
+from utils import get_dp_folders
+from GPT4V_backbone import GPT4V
+
+@dataclass
+class PipelineParameters:
+
+    dataset_folder: Path
+    output_file: Path
+    openai_token: str
+    instructs: dict
+
+
+def prepare_pipeline(config_path, out_filename):
+
+    random.seed(42)
+
+    config = OmegaConf.load(config_path)
+    openai_token_file = config.openai_token_file
+    dataset_folder = Path(config.dataset_final)
+    out_folder = Path(config.out_folder)
+    output_file = out_folder / pipline_parameters.instructs
+
+    os.makedirs(out_folder, exist_ok=True)
+
+    with open(openai_token_file, "r") as f:
+        openai_token = f.read()
+
+    with open("prompts/plot_gen.json", 'r') as f:
+        instructs = f.read()
+        instructs = json.loads(instructs)
+
+    return PipelineParameters(dataset_folder, output_file, openai_token, instructs)
+
+
+
+def generate_plotting_request(dp_folder: Path, instructs):
+
+    "Request to ask model to write a code for plotting. Add dataframe description"
+
+    df_descr_file = dp_folder / "data_descr.txt"
+    task_file = dp_folder / "task.json"
+
+    with open(task_file, 'r') as f:
+        task_dict = json.load(f)
+
+    with open(df_descr_file, 'r') as f:
+        df_descr = f.read()
+
+    task = ""
+    for i, (task_type, task_part) in enumerate(task_dict.items()):
+        task += f"{i}.task_part\n"
+        if task_type == "data description":
+            task += instructs["data instruct"] + df_descr + "\n"
+
+    task = task.replace("\n\n", "\n")
+
+    return task
+
+if __name__ == "__main__":
+
+    config_path = "configs/config.yaml"
+    pipline_parameters = prepare_pipeline(config_path,"gpt_plot.jsonl")
+
+    gpt4v = GPT4V(api_key=pipline_parameters.openai_token, system_prompt=pipline_parameters.instructs["system prompt"])
+    responses = []
+
+    dp_folders = get_dp_folders(pipline_parameters.dataset_folder)
+    # dp_folders = dp_folders[:1]
+    # dp_folders = random.sample(dp_folders, 20)
+    for i, dp_folder in tqdm(enumerate(dp_folders), total=len(dp_folders)):
+
+        index = int(dp_folder.name)
+        task = generate_plotting_request(dp_folder, pipline_parameters.instructs)
+        response = gpt4v.make_request(task)
+
+        if response is None:
+            print(f"Skipping dp {index}")
+            continue
+
+        response["id"] = index
+        responses.append(response)
+
+        with open(pipline_parameters.output_file, "a") as f:
+            json.dump(response, f)
+            f.write("\n")
